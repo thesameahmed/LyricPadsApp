95fb37e936a21f3af8dcb0be13c5a613b90e56e6
thesameahmed <104497035+thesameahmed@users.noreply.github.com>
2022-12-09 11:40:57 -0500

    Create LyricPadsApp.swift
    Created Main App Page For Swift-UI LyricPadsApp

---
+++ LyricPadsApp.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+// Main App Page For SwiftUI-Lyric-Pads-App //
+
+
+//Planning to import AVFoundation & Handle Audio Functionality on other App Views //
+
+
+
+
+@main
+
+struct SpeakUpApp: App {
+
+
+    var body: some Scene {
+    
+   
+        WindowGroup {
+        
+        
+        
+            ContentView()
+            
+            
+                           
+        }
+             
+        
+    }
+       
+    
+}
